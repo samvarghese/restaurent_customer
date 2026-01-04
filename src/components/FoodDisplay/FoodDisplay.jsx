5b5a22e533d54a646519c16dc1e81590684a8229
samvarghese38 <sam.varghese32@gmail.com>
2026-01-04 21:07:22 +0930

    update

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useContext } from 'react'
+import './FoodDisplay.css';
 import { StoreContext } from '../../context/StoreContext'
 
 const FoodDisplay = () => {
@@ -19,12 +20,12 @@ const FoodDisplay = () => {
                 
                   <div key ={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center">
                       <div className="card product-card">
-                    <img src={food.imageUrl} className="card-img-top" alt="Product Image" width={200} height={150}/>
+                    <img src={food.imageUrl} className="card-img-top product-image" alt="Product Image"/>
                     <div className="card-body">
                         <h5 className="card-title product-title">{food.name}</h5>
                   
                         <div className="d-flex justify-content-between align-items-center mb-2">
-                            <span className="product-price">{food.price}</span>
+                            <span className="product-price">AUD${food.price}</span>
                             <div className="rating">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
